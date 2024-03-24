158ab35406b631628824071926c7ca3a1a9a0f24
Babchinsky <113105752+Babchinsky@users.noreply.github.com>
2024-03-24 22:08:59 +0200

    Add files via upload

---
+++ HW 19.03.2024 Angular/project2/src/app/articles.ts
@@ -0,0 +1,27 @@
+export const articles = [
+  {
+    src: 'assets/images/1.jpg',
+    title: ' Free Entertaimment',
+    text: ' Our advanced, touch-screen digital entertainment system puts you in charge of your entertainment options. ',
+  },
+  {
+    src: 'assets/images/2.jpg',
+    title: 'Turn Dreamming',
+    text: 'Customize your ideal vacation to 300+ destinations worldwide with Delta Vacations. ',
+  },
+  {
+    src: 'assets/images/3.jpg',
+    title: 'Gift Card',
+    text: 'Give the Gift of Go™ and let them explore more than 300 destinations worldwide. Terms apply. Shop now. ',
+  },
+  {
+    src: 'assets/images/4.jpg',
+    title: 'New Destination',
+    text: 'Discover exciting new destinations and experiences with our latest travel packages.',
+  },
+  {
+    src: 'assets/images/5.jpg',
+    title: 'Exclusive Offer',
+    text: 'Take advantage of our exclusive offers and discounts for your next getaway.',
+  },
+];
